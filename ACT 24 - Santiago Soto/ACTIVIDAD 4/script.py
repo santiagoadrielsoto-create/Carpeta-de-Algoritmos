4b4d54fa2bad25ec64384420d627dd87675aeb2d
santiagoadrielsoto-create <santiagoadrielsoto@gmail.com>
2026-08-18 13:26:04 -0300

    Agrego ACT 24

--- ACT 24 - Santiago Soto/ACTIVIDAD 4/script.py
+++ ACT 24 - Santiago Soto/ACTIVIDAD 4/script.py
@@ -1,4 +1,4 @@
-""" CONSIGNA - PUNTO 4:
+"""4:
  Una empresa de e-commerce utiliza drones autónomos para realizar entregas a domicilio
  y necesita rastrear las coordenadas geográficas de sus rutas de vuelo.
  Diseñar un diccionario donde la Clave sea el identificador único del dron (ej: "DRON-01")
@@ -12,23 +12,6 @@
  3. Ruta más larga: Determinar y mostrar el identificador del dron que tiene la mayor
     cantidad de paradas registradas en su ruta de vuelo (la lista con mayor cantidad de elementos).
 """
-
-Acá tenés la versión del ejercicio de los drones adaptada sin usar .items(), sin enumerate() y recorriendo todo con índices clásicos range(len(...)) para usar siempre una sola variable en cada bucle for:
-
-Python
-# CONSIGNA - PUNTO 4:
-# Una empresa de logística utiliza drones para entregar paquetes en zonas urbanas.
-# Crear un diccionario donde la Clave sea el ID del dron (ej: "DRON-01")
-# y el Valor sea una lista de tuplas con las coordenadas GPS de sus paradas:
-# [(latitud, longitud)].
-# Desarrollar las siguientes funciones:
-# 1. Cargar drones: Registrar 3 drones. Para cada uno, solicitar cuántas paradas
-#    hará en su ruta y pedir las coordenadas (latitud y longitud) de cada una.
-# 2. Imprimir rutas: Mostrar en pantalla el recorrido completo de cada dron
-#    indicando el número de parada y sus coordenadas.
-# 3. Ruta más larga: Determinar cuál es el dron que tiene la mayor cantidad de paradas.
-
-
 def cargar():
     drones = {}
 
@@ -85,9 +68,7 @@ def ruta_mas_larga(drones):
             dron_mas_largo = id_dron
 
     if dron_mas_largo != "":
-        print(
-            "El dron con la ruta mas larga es" ,dron_mas_largo,"conn",max_paradas,"paradas registradas.",
-        )
+        print( "El dron con la ruta mas larga es" ,dron_mas_largo,"conn",max_paradas,"paradas registradas.",)
 
 
 drones = cargar()
